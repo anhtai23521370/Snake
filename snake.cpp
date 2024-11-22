7b31206537bb5021d533bba19b1b9821fe9e798c
NgocHan1211 <23520437@gm.uit.edu.vn>
2024-11-22 11:05:43 +0700

    Update snake.cpp

--- snake.cpp
+++ snake.cpp
@@ -77,9 +77,9 @@ public:
         
         for (int i = DoDai - 1; i > 0; i--)
             A[i] = A[i-1];
-        if (Huong==0) A[0].x = A[0].x + 1;
-        if (Huong==1) A[0].y = A[0].y + 1;
-        if (Huong==2) A[0].x = A[0].x - 1;
+        if (Huong == 0) A[0].x = A[0].x + 1;
+        if (Huong == 1) A[0].y = A[0].y + 1;
+        if (Huong == 2) A[0].x = A[0].x - 1;
         if (Huong==3) A[0].y = A[0].y - 1;
 
     }
